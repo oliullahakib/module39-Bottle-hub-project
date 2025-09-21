baf9e8cc8b813af7069aa4f451555ab3388e1577
oliullahakib <oliullahakib@gmail.com>
2025-09-21 16:33:33 +0600

    get and set to localStorage logics is added and working on delete logics

--- src/componets/Bottles/Bottles.jsx
+++ src/componets/Bottles/Bottles.jsx
@@ -1,11 +1,34 @@
-import React, { use,useState } from 'react'
+import React, { use,useState,useEffect } from 'react'
 import Bottle from '../Bottle/Bottle'
 import "./Bottles.css"
 import Carts from '../Carts/Carts'
 const Bottles = ({bottlePromiss}) => {
  const bottles = use(bottlePromiss)
  const [cart, setCart] = useState([])
+ useEffect(() => {
+const cartIdArr = getFromLs();
+const cartBottle =cartIdArr.map(id=>{
+    return bottles.find(bottle => bottle.id ===id)
+})
+setCart(cartBottle)
+ }, [bottles])
+ 
+ const getFromLs=()=>{
+    const cartIdString = localStorage.getItem("cartId");
+    if(cartIdString){
+        let cartId=JSON.parse(cartIdString)
+        return cartId
+    }
+    return []
+ }
+ const addToLs =(id)=>{
+    const cartIdArr = getFromLs();
+    cartIdArr.push(id);
+    const cartIdString = JSON.stringify(cartIdArr)
+    localStorage.setItem("cartId",cartIdString)
+ }
  const handleCart =(bottle)=>{
+    addToLs(bottle.id)
     let newCart = [...cart,bottle]
    setCart(newCart)
  }
